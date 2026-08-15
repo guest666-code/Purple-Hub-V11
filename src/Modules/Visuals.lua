07a0a1607fffdee4e940ac1e23701d3a918858dc
guest666-code <efemertdede9@gmail.com>
2026-08-15 03:52:50 +0300

    Update Visuals.lua

--- src/Modules/Visuals.lua
+++ src/Modules/Visuals.lua
@@ -1,1 +1,15 @@
+-- Purple Hub V11 | Visuals Module
+local Visuals = {}
 
+function Visuals.Init(Settings)
+    -- Görsel ayarları ve katmanları burada yönetebilirsin
+    print("Visuals Modülü Yüklendi.")
+end
+
+function Visuals.Update(Settings)
+    if Settings.Visuals.ESP then
+        -- ESP mantığı veya istemci içi çizim güncellemeleri
+    end
+end
+
+return Visuals
