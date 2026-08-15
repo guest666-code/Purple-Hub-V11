c36989c75b84aa5e34cb310149af0275e0ba5f3f
guest666-code <efemertdede9@gmail.com>
2026-08-15 03:53:06 +0300

    Update Combat.lua

--- src/Modules/Combat.lua
+++ src/Modules/Combat.lua
@@ -1,1 +1,15 @@
+-- Purple Hub V11 | Combat Module
+local Combat = {}
+
+function Combat.Init(Settings)
+    print("Combat Modülü Yüklendi.")
+end
+
+function Combat.Process(Settings)
+    if Settings.Combat.AutoAim then
+        -- Otomatik hedefleme veya kilitlenme mantığı
+    end
+end
+
+return Combat
 
